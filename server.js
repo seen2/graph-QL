26efebd6b4d536b86fe583a4f46c902cf9f72649
seen2 <tiwarietc@gmail.com>
2022-08-22 08:51:31 +0530

    Simple Use Case

--- server.js
+++ server.js
@@ -1,46 +1,146 @@
-import { ApolloServer,gql } from 'apollo-server';
+import { ApolloServer, gql } from "apollo-server";
 
-import {ApolloServerPluginLandingPageLocalDefault} from'apollo-server-core';
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
+
+// import { products,categories,reviews} from "./src/data";
+const products = [
+  {
+    id: "53a0724c-a416-4cac-ae45-bfaedce1f147",
+    name: "Steel Pot",
+    description: "Silver steel pot that is perfect for cooking",
+    quantity: 230,
+    price: 42.44,
+    image: "img-1",
+    onSale: false,
+  },
+  {
+    id: "c2af9adc-d0b8-4d44-871f-cef66f86f7f6",
+    name: "Salad Bowl",
+    description: "Round wooden bowl perfect for tossing and making salads",
+    quantity: 33,
+    price: 53.5,
+    image: "img-2",
+    onSale: false,
+  },
+  {
+    id: "2c931e7e-510f-49e5-aed6-d6b44087e5a1",
+    name: "Spoon",
+    description: "Small and delicate spoon",
+    quantity: 4266,
+    price: 1.33,
+    image: "img-3",
+    onSale: true,
+  },
+  {
+    id: "404daf2a-9b97-4b99-b9af-614d07f818d7",
+    name: "Shovel",
+    description: "Grey rounded shovel for digging",
+    quantity: 753,
+    price: 332,
+    image: "img-4",
+    onSale: false,
+  },
+  {
+    id: "6379c436-9fad-4b3f-a427-2d7241f5c1b1",
+    name: "Fertilizer",
+    description: "Nitrogen based fertitlizer",
+    quantity: 53453,
+    price: 23.11,
+    image: "img-5",
+    onSale: true,
+  },
+  {
+    id: "f01bcdec-6783-464e-8f9e-8416830f7569",
+    name: "Basketball",
+    description: "Outdoor or indoor basketball",
+    quantity: 128,
+    price: 59.99,
+    image: "img-6",
+    onSale: true,
+  },
+  {
+    id: "a4824a31-5c83-42af-8c1b-6e2461aae1ef",
+    name: "Golf Clubs",
+    description: "Good for golfing",
+    quantity: 3,
+    price: 427.44,
+    image: "img-7",
+    onSale: false,
+  },
+  {
+    id: "b553085a-a7e0-4c9b-8a12-f971919c3683",
+    name: "Baseball Gloves",
+    description: "Professional catcher gloves",
+    quantity: 745,
+    price: 77.0,
+    image: "img-8",
+    onSale: true,
+  },
+  {
+    id: "47bf3941-9c8b-42c0-9c72-7f3985492a5b",
+    name: "Soccer Ball",
+    description: "Round ball",
+    quantity: 734,
+    price: 93.44,
+    image: "img-9",
+    onSale: false,
+  },
+];
 
 //Scalar Types: (String,Int,Float,Boolean) or null
 //to  avoid null use(!) like String!
 
+// const typeDefs = gql`
+//   type Query {
+//     hello: String,
+//     numberOfAnimals:Int!,
+//     price:Float,
+//     isCool:Boolean
+//   }
+// `
+
 const typeDefs = gql`
   type Query {
-    hello: String,
-    numberOfAnimals:Int!,
-    price:Float,
-    isCool:Boolean
+    hello: [String!]!
+    products: [Product!]
+    product(id:ID!):Product
   }
-`
+
+  type Product { #object Type to get it in query we have to define what we want from the object type
+    id: String!
+    name: String!
+    description: String!
+    quantity: Int!
+    price: Float!
+    image: String!
+    onSale: Boolean!
+  }
+`;
 
 const resolvers = {
   Query: {
-    hello: () => "World",
-    numberOfAnimals:()=>null,
-    price:()=>201.33,
-    isCool:()=>false
+    hello: () => ["Hello", "My", "Dear"],
+    products: () => products,
+    product:(parent,args,context) =>{
+      const productId=args.id;
+      return products.find(product=>product.id===productId)
+    }
   },
 };
-
-
-
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   csrfPrevention: true,
-  cache: 'bounded',
+  cache: "bounded",
   /**
    * What's up with this embed: true option?
    * These are our recommended settings for using AS;
    * they aren't the defaults in AS3 for backwards-compatibility reasons but
    * will be the defaults in AS4. For production environments, use
    * ApolloServerPluginLandingPageProductionDefault instead.
-  **/
-  plugins: [
-    ApolloServerPluginLandingPageLocalDefault({ embed: true }),
-  ],
+   **/
+  plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
 });
 
 server.listen().then(({ url }) => {
